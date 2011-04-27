cae34b5ebfefcef6e0c892f2ea3a2699c1391f86
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2011-04-27 18:31:45 +0400

    version

--- ext/ruby_debug/ruby_debug.c
+++ ext/ruby_debug/ruby_debug.c
@@ -9,7 +9,7 @@
 #include <insns_info.inc>
 #include "ruby_debug.h"
 
-#define DEBUG_VERSION "0.11.27"
+#define DEBUG_VERSION "0.11.28"
 
 #define FRAME_N(n)  (&debug_context->frames[debug_context->stack_size-(n)-1])
 #define GET_FRAME   (FRAME_N(check_frame_number(debug_context, frame)))
