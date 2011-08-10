74c55a8db439f2db60fc9758b63be0c815ea70ac
Dennis Ushakov <dennis.ushakov@gmail.com>
2011-08-10 20:50:36 +0400

    add some rvm lovin'

--- ext/ruby_debug/extconf.rb
+++ ext/ruby_debug/extconf.rb
@@ -1,11 +1,6 @@
 require "mkmf"
 require "ruby_core_source"
 
-if RUBY_VERSION < "1.9"
-  STDERR.print("Ruby version is too old\n")
-  exit(1)
-end
-
 hdrs = proc {
   have_struct_member("rb_method_entry_t", "body", "method.h")
   have_header("vm_core.h") and have_header("iseq.h") and have_header("insns.inc") and 
@@ -21,7 +16,17 @@ if RUBY_REVISION >= 26959 # rb_iseq_compile_with_option was added an argument fi
 end
 
 dir_config("ruby")
-if !Ruby_core_source::create_makefile_with_core(hdrs, "ruby_debug")
+name = "ruby_debug"
+if (ENV['rvm_ruby_string'])
+  dest_dir = Config::CONFIG["rubyhdrdir"]
+  with_cppflags("-I" + dest_dir) {
+    if hdrs.call
+      create_makefile(name)
+      exit 0
+    end
+  }
+end
+if !Ruby_core_source::create_makefile_with_core(hdrs, name)
   STDERR.print("Makefile creation failed\n")
   STDERR.print("*************************************************************\n\n")
   STDERR.print("  NOTE: For Ruby 1.9 installation instructions, please see:\n\n")
